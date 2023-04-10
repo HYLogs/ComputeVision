edc766aab9048c19b6bced67abbde9e6deec8ff5
HanYoung Lee <dkdlel644@naver.com>
2023-04-10 20:42:22 +0900

    과제2 filter2D 필터 수정

--- 과제2/filter2D.py
+++ 과제2/filter2D.py
@@ -4,9 +4,11 @@ import numpy as np
 img = cv2.imread("img/book.png")
 
 if img is not None:
-    kernel = np.ones((3, 3), dtype=np.float64)
-    kernel[1, :] = 0
-    kernel[2, :] = -1
+    kernel = np.array([[-1, -2, -3, -2, -1],
+                       [-2, -4, -6, -4, -2],
+                       [0, 0, 0, 0, 0],
+                       [2, 4, 6, 4, 2],
+                       [1, 2, 3, 2, 1]]) # solbel
     print(kernel)
 
     filterd_img = cv2.filter2D(img, -1, kernel) # -1은 원본 데이터 타입을 사용
